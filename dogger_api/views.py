9d7d21e16f670c418d9141fcc037f9877de39b40
Jaime Chavez <alexchavez234@hotmail.com>
2021-11-12 12:52:23 -0500

    added test api view

--- dogger_api/views.py
+++ dogger_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class HelloApiView(APIView):
+    """Test API view"""
+    def get(self, request, format=None):
+        """Return a list of APIView features"""
+        an_api_view = [
+            'Use HTTP methods as function (get, post, patch, put, delete)',
+            'Is similar to a traditional django view'
+        ]
+
+        return Response({'message': 'Hello!', 'an_apiview': an_api_view})
+
+
